5ddb02ec248ead0802ef9ef0c3fd99bfb113738a
Dennis Mullen <dkmullen@gmail.com>
2017-04-13 22:43:01 -0400

    Work on individual record page, linking up phone and email, etc

--- client-side/scripts/app.js
+++ client-side/scripts/app.js
@@ -1,19 +1,32 @@
 /*jshint esversion: 6 */
 
 (function() {
-  var app = angular.module('directory', []);
-  var baseUrl = 'http://localhost:3050';
+  const app = angular.module('directory', []);
+  const baseUrl = 'http://localhost:3050';
 
   app.controller('MemberListController', [ '$http', function($http) {
-    var directory = this;
+    let directory = this;
     directory.members = [];
 
-    $http.get(baseUrl + '/members/58f005045a8f401295742472')
-      .then(function(data){
+    $http.get(baseUrl + '/members')
+      .then((data) => {
         directory.members = data;
-        console.log(directory.members);
       })
-      .catch(function(err){
+      .catch((err) => {
+        console.log('You got knocked the F out, man!');
+      });
+  }]);
+
+  app.controller('MemberRecordController', [ '$http', function($http) {
+    let record = this;
+    record.member = [];
+
+    $http.get(baseUrl + '/members/58f005045a8f401295742474')
+      .then((data) => {
+        record.member = data;
+        console.log(record.member.data.firstName);
+      })
+      .catch((err) => {
         console.log('You got knocked the F out, man!');
       });
   }]);

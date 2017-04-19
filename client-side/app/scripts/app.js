78f46d8169fb20a3bed935fc827e9c4c5e7fb1ed
Dennis Mullen <dkmullen@gmail.com>
2017-04-18 21:40:16 -0400

    Fix views, eliminating templateUrl for now, replace with string

--- client-side/app/scripts/app.js
+++ client-side/app/scripts/app.js
@@ -1,8 +1,32 @@
 /*jshint esversion: 6 */
 
 (function() {
-  const app = angular.module('directoryApp', []);
+  const app = angular.module('directoryApp', ['ui.router']);
   const baseUrl = 'http://localhost:3050';
+
+  app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('main', {
+      url: '/',
+      views: {
+        'header': {
+          template : '<h3>My Header</h3>'
+        },
+        'content': {
+          template : `<div ng-repeat="member in directory.members.data | orderBy: 'lastName'">
+                <a href="members/{{member._id}}">
+                <img ng-src={{member.image.thumb}} alt={{member.firstName}} />
+                <h3>{{member.firstName}} {{member.lastName}}</h3></a></div>`,
+        },
+        'footer': {
+          template: '<h3>My Footer'
+        }
+      }
+    });
+
+    $urlRouterProvider.otherwise('/');
+  });
 
   app.controller('MemberListController', [ '$http', function($http) {
     this.test = function(id) {

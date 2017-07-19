daed0e6b34790257ee6652f52e12b3453199557d
Dennis Mullen <dkmullen@gmail.com>
2017-07-19 11:05:42 -0400

    Reversed gulp addition

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/*jshint esversion: 6 */
+
+const gulp = require('gulp'),
+  postcss = require('gulp-postcss'), // main package to add post-processing to css
+  autoprefixer = require('autoprefixer'), // adds vendor prefixes when needed
+  cssvars = require('postcss-simple-vars'), // processes vars into valid css
+  nested = require('postcss-nested'), // processes nested css into valid css
+  cssImport = require('postcss-import'), // Allows for importing css as modules
+  mixins = require('postcss-mixins'); // mixins are reusable bits of code
+
+  gulp.task('styles', () => {
+    return gulp.src('./front-end/public/temp/styles.css') // Get this file...
+      .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) // ...run it thru these...
+      .on('error', function(errorInfo) { // Crashes on fat-arrow function
+        console.log(errorInfo.toString());
+        this.emit('end'); // End this task gracefully on error
+      })
+      .pipe(gulp.dest('./front-end/public/stylesheets/mystyles.css')); // ...then put it here.
+  });

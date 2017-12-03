9fa6eaa069dc20c03edc4a052f07b4108ad19d62
Dennis Mullen <dkmullen@gmail.com>
2017-12-03 18:34:31 -0500

    Fixed an error in config file

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 /*jshint esversion: 6 */
 
-const uri = process.env.MONGOCLOUD_DIR_CONNECT_STR ||
-  'mongodb://localhost:27017/directory-app';
+const uri = process.env.MONGOCLOUD_DIR_CONNECT_STR,
+  uri2 = 'mongodb://localhost:27017/directory-app';
 
 module.exports = {
     'secret': 'fakesecret',
-    'mongoUrl' : uri
+    'mongoUrl' : uri2
 };
 // To start mongo on Windows - "C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe"
